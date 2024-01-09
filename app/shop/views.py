c9df464fad1e4f43e1c61fe6a99f3179ee0f65cc
BykovVik <piton20091986@gmail.com>
2024-01-09 23:20:20 +0300

    add cart button

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404
 from .models import Category, Product
+from cart.form import CartAddProductForm
 
 def product_list(request, category_slug=None):
 
@@ -24,12 +25,14 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
 
     product = get_list_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
 
     return render(
         request,
         'shop/product/detail.html',
         {
-            'product': product[0]
+            'product': product[0],
+            'cart_product_form': cart_product_form
         }
     )
 
